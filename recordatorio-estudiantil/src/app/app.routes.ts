f82c9f6233e7df5552f1ca2e84fefe673e591d10
Nelly <burgosn871@gmail.com>
2026-02-11 20:06:05 -0300

    Remove comment about register.ts import
    Removed unnecessary comment regarding register.ts import.

--- recordatorio-estudiantil/src/app/app.routes.ts
+++ recordatorio-estudiantil/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { AddTaskComponent } from './components/add-task/add-task';
 
-// Si tu archivo se llama 'register.ts', la importación debe terminar así:
 import { RegisterComponent } from './components/register/register'; 
 
 export const routes: Routes = [
