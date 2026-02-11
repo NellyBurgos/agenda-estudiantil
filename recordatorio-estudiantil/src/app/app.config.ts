02e7a3ca45209937ae5f1f9154eabe550b587f65
Nelly <burgosn871@gmail.com>
2026-02-11 20:04:39 -0300

    Clean up Firebase configuration in app.config.ts
    Removed unused Firebase initialization code and comments.

--- recordatorio-estudiantil/src/app/app.config.ts
+++ recordatorio-estudiantil/src/app/app.config.ts
@@ -7,13 +7,6 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
-
-
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-//  web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDSr5-Q1JLJu1eZv_QOijr6PYY1E6RmjXc",
   authDomain: "recordatorioestudiantil.firebaseapp.com",
@@ -23,8 +16,6 @@ const firebaseConfig = {
   appId: "1:925121140468:web:b4ae8518b918d63381b0a1"
 };
 
-//// Initialize Firebase
-//const app = initializeApp(firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
